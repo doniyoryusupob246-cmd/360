8dc8c3c7da700a710d9094d2b107cc07656db64d
Doniyor <doniyoryusupob246@gmail.com>
2026-01-09 12:09:17 +0300

    d

--- data.js
+++ data.js
@@ -29,7 +29,7 @@ var APP_DATA = {
           "yaw": -3.11333115279386,
           "pitch": 0.04367573856264784,
           "rotation": 0,
-          "target": "1-360-1"
+          "target": "2-000"
         }
       ],
       "infoHotspots": []
@@ -59,6 +59,46 @@ var APP_DATA = {
         "fov": 1.2599180821480807
       },
       "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-000",
+      "name": "000",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2118348497875484,
+          "pitch": 0.2598725092565459,
+          "rotation": 0,
+          "target": "1-360-1"
+        },
+        {
+          "yaw": 0.7423056778353363,
+          "pitch": 0.1790799428795946,
+          "rotation": 0,
+          "target": "0-360"
+        }
+      ],
       "infoHotspots": []
     }
   ],
